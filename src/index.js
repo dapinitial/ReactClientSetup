fd2668d9efa37bf42e12ed06e6aa9e171ca4673c
David Puerto <me@davidpuerto.com>
2017-12-08 01:42:41 -0800

    root IndexRoute and auto-authenticating users

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import reduxThunk from "redux-thunk";
+import { AUTH_USER } from "./actions/types";
 
 import App from "./components/app";
 import Feature from "./components/feature";
+import Welcome from "./components/welcome";
 import Signin from "./components/auth/signin";
 import Signup from "./components/auth/signup";
 import Signout from "./components/auth/signout";
@@ -14,11 +16,19 @@ import RequireAuth from "./components/auth/require_auth";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem("token");
+// If we have a token consider the user to be signed in.
+if (token) {
+  // we need to update the application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
         <Route path="/signin" component={Signin} />
         <Route path="/signout" component={Signout} />
         <Route path="/signup" component={Signup} />
